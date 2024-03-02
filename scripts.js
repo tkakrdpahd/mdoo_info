885ae5bfae491eb777a85ca9a42b3c59d221a029
tkakrdpahd <89321156+tkakrdpahd@users.noreply.github.com>
2024-03-02 17:43:25 -0500

    update
    update

--- scripts.js
+++ scripts.js
@@ -1,1 +1,41 @@
 // scripts.js
+document.addEventListener('DOMContentLoaded', initializePage);
+function initializePage() {
+    setupPageNavigation();
+}
+
+function setupPageNavigation() {
+    const ids = [
+        'index', 'profile', 'ArtDescriptions', 'DevDescriptions', 'contact'
+    ];
+
+    ids.forEach(id => {
+        const element = document.getElementById(id);
+        if (element) {
+            element.addEventListener('click', event => {
+                event.preventDefault();
+                if (id === 'index') {
+                    window.location.href = '../index.html';
+                } else {
+                    const currentPage = window.location.pathname.split('/').pop();
+                    if (currentPage === 'index.html' || currentPage === '') {
+                        window.location.href = './html/detailPage.html';
+                    }
+                    readAndWrite(id);
+                }
+            });
+        }
+    });
+}
+
+
+function readAndWrite(pageId){
+    fetch("../json/" + pageId + ".json")
+    .then(response => response.json())
+        .then(json => {
+
+    })
+    .catch(error => {
+        console.error('Error fetching JSON:', error);
+    });
+}
