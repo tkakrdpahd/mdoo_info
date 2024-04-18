6f815869d34486e66ce092b3f81f4459bb01f621
tkakrdpahd <89321156+tkakrdpahd@users.noreply.github.com>
2024-04-18 11:31:15 -0400

    update
    update

--- scripts.js
+++ scripts.js
@@ -1,37 +1,58 @@
 document.addEventListener('DOMContentLoaded', initializePage);
 
 function initializePage() {
-    navigationRedirect();
+    let newHeader = new Header();
+    newHeader.navigationRedirect();
 }
 
-function navigationRedirect() {
-    var ids = ['index', 'curriculumVitae', 'linkedIn', 'enBlog', 'krBlog', 'artStation', 'gitHub', 'itchIo'];
+class Header {
+    constructor() {
+        this.language = 'ko';
+        this.ids = ['index', 'curriculumVitae', 'linkedIn', 'enBlog', 'krBlog', 'artStation', 'gitHub', 'itchIo'];
+        this.pageMap = {
+            'index': 'https://www.mdoo.info',
+            'curriculumVitae': 'https://ftp.mdoo.info/Documents/Resume.pdf',
+            'linkedIn': 'https://www.linkedin.com/in/minseok-doo/',
+            'enBlog': 'https://blog.mdoo.info',
+            'krBlog': 'https://blog.mdoo.pe.kr',
+            'artStation': 'https://mdoo_pri.artstation.com',
+            'gitHub': 'https://github.com/tkakrdpahd',
+            'itchIo': 'https://mdoo12.itch.io'
+        };
+    }
 
-    var pageMap = {
-        'index': 'https://www.mdoo.info',
-        'curriculumVitae': 'https://ftp.mdoo.info/Documents/Resume.pdf',
-        'linkedIn': 'https://www.linkedin.com/in/minseok-doo/',
-        'enBlog': 'https://blog.mdoo.info',
-        'krBlog': 'https://blog.mdoo.pe.kr',
-        'artStation': 'https://mdoo_pri.artstation.com',
-        'gitHub': 'https://github.com/tkakrdpahd',
-        'itchIo': 'https://mdoo12.itch.io'
-    };
-
-    ids.forEach(function(id) {
-        var element = document.getElementById(id);
-        if (element) {
-            element.addEventListener('click', function(event) {
-                event.preventDefault();
-                var url = pageMap[id];
-                if (url) {
-                    window.location.href = url;
-                }
-            });
+    navigationRedirect() {
+        this.ids.forEach(id => {
+            const element = document.getElementById(id);
+            if (element) {
+                element.addEventListener('click', event => {
+                    event.preventDefault();
+                    const url = this.pageMap[id];
+                    if (url) {
+                        window.location.href = url;
+                    }
+                });
+            }
+        });
+    }
+    
+    // this is language button eventListener
+    langButtonEventListener() {
+        if (!localStorage.getItem("language")) {
+            this.updateLanguageInfo("ko");
         }
-    });
-}
-
-function languageButtonEvent() {
-
+    
+        document.getElementById('korean').addEventListener('click', () => {
+            this.updateLanguageInfo("ko");
+        });
+    
+        document.getElementById('english').addEventListener('click', () => {
+            this.updateLanguageInfo("en");
+        });
+    }
+    
+    updateLanguageInfo(language) {
+        localStorage.setItem("language", language);
+        location.reload();
+    }
 }
