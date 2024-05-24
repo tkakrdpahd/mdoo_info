95479a824a1f128c70fbbb13514ee3c691f251d5
tkakrdpahd <89321156+tkakrdpahd@users.noreply.github.com>
2024-05-24 10:56:20 +0900

    update
    update

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,12 +1,41 @@
 import './Nav.css';
+import React, { useEffect } from 'react';
 
-function Nav(){
+function setCssForMobile() {
+    const h4Elements = document.querySelectorAll('nav li h4');
+    h4Elements.forEach(h4 => {
+        h4.addEventListener('click', event => {
+            if (window.innerWidth <= 1079) {
+                const listItems = h4.parentElement.querySelectorAll('ul');
+                listItems.forEach(ul => {
+                    ul.style.display = 'flex';
+                });
+
+                const button = document.getElementById('ulButton');
+                if (button) {
+                    button.style.display = 'block';
+                    button.onclick = () => {  // Add a click event listener to the button
+                        listItems.forEach(ul => {
+                            ul.style.display = 'none';
+                        });
+                        button.style.display = 'none';
+                    };
+                }
+            }
+        });
+    });
+}
+
+function Nav() {
+    useEffect(() => {
+        setCssForMobile();
+    }, []);
+
     const handleButtonClick = () => {
-        // 여기에 버튼 클릭 시 수행할 동작을 작성합니다.
-        console.log('Button clicked!');
+        setCssForMobile();
     };
 
-    return(
+    return (
         <nav>
             <ul>
                 {/* button for mobile */}
@@ -36,8 +65,14 @@ function Nav(){
                         <li><a href="https://mdoo12.itch.io">Itch.io</a></li>
                     </ul>
                 </li>
+                <li id="shop">
+                    <h4>Shop</h4>
+                    <ul className="subMenu">
+                        <li><a href="https://kmong.com/gig/553495">Kmong</a></li>
+                    </ul>
+                </li>
             </ul>
-        </nav> 
+        </nav>
     );
 }
 
