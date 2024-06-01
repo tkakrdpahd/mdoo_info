8989d74b817ccf2b1aebdb6adf3e4204bf17c04a
tkakrdpahd <89321156+tkakrdpahd@users.noreply.github.com>
2024-06-01 09:06:29 +0900

    update
    update

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,6 +1,22 @@
+import './Main.css';
+
 function Main() {
     return(
-        <div></div>
+        <div>
+            <article>
+            <h2>Welcome to my webpage | 제 홈페이지에 오신것을 환영합니다</h2>
+            <h3>You know? This is ridiculous.<br/>Scribble, scribble, scribble...</h3>
+            </article>
+            {/* <article class="artwork">
+            <div class="mainArtwork">
+                <img src="https://cdna.artstation.com/p/assets/images/images/070/687/252/large/minseok-doo-img-9302.jpg?1703178582" />
+                <p></p>
+                <h4>Title: Where is yout home?</h4>
+                <h4>Year: 2023</h4>
+                <h4>Medium: Soapstone</h4>
+            </div>
+            </article> */}
+        </div>
     );
 }
 
